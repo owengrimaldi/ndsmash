a9cd68f0c82ab53d615df92a3224834058aada72
Owen Grimaldi <ogrimald@db8.cse.nd.edu>
2023-12-05 12:42:02 -0500

    front end progress

--- webapp.py
+++ webapp.py
@@ -9,7 +9,7 @@ to the front-end demo.html page.
 
 '''
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect, request, session
 from mysql.connector import connect, Error
 from graphqlclient import GraphQLClient
 import json, time, os
@@ -18,6 +18,7 @@ import random
 
 #Flask app initialization
 app = Flask(__name__, template_folder='./', static_url_path='/static')
+app.secret_key = "penis"
 database = connect(host="localhost", user="ogrimald", password="goirish", database="ogrimald")
 cursor = database.cursor(buffered=True)
 cursor.execute("use ogrimald")
@@ -30,6 +31,11 @@ def ndQuery():
     cursor.execute("select username from players where players.prefix = 'ND'")
     queriedPlayers = [x[0] for x in cursor]
     return queriedPlayers
+
+def usernameQuery():
+    cursor.execute("select username from players;")
+    players = [x[0] for x in cursor]
+    return players
 
 #Counts total # of entries in each table
 def getNumbers():
@@ -60,12 +66,41 @@ def index():
     #sends data to the html template where it is displayed to user
     return render_template("./demo.html", players=queriedPlayers, numPlayers=queriedResult[0], numTournaments = queriedResult[1], numSets = queriedResult[2], tournaments = tournaments)
 
+
 @app.route("/", methods=["GET", "POST"])
 def home():
     namelogov3Ratio = 4.253392812
     namelogov4Ratio = 1.904075714
 
-    return render_template("./home.html", width=190, height=190)
+    players = usernameQuery()
+
+    errorAlert = ""
+
+
+    if request.method == 'POST':
+        searchbar = request.form.get("searchbar")
+        playerURL = url_for('players', searchbar=searchbar)
+        tournamentURL = url_for('tournaments', searchbar=searchbar)
+    else:
+        playerURL = url_for('players')
+        tournamentURL = url_for('tournaments')
+
+
+    return render_template("./home.html", languages=players, playerURL=playerURL, tournamentURL=tournamentURL, errorAlert=errorAlert, width=160, height=160)
+
+
+@app.route("/players", methods=["GET", "POST"])
+def players():
+    searchbar =  request.form.get("searchbar")
+
+    return render_template("./players.html", searchbar=searchbar)
+
+
+@app.route("/tournaments", methods=["GET", "POST"])
+def tournaments():
+    searchbar =  request.form.get("searchbar")
+
+    return render_template("./tournaments.html", searchbar=searchbar)
 
 
 
